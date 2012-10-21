c04e3e1b2e74312df60db0fe6f91bbd030e2c803
Max00355 <max00355@gmail.com>
2012-10-21 11:18:52 -0700

    Update PyColors.py

--- PyColors.py
+++ PyColors.py
@@ -8,8 +8,9 @@ class windows():
   def __init__(self):
     self.colors = {'black':'0', 'blue':'1', 'green':'2', 'aqua':'3', 'red':'4','purple':'5','yellow':'6','white':'7','gray':'8','light_blue':'9','light_green':'a','light_aqua':'b','light_red':'c','light_purple':'d','light_yellow':'e','bright_white':'f'}
   def color(self, textcolor=None, background=None):
-    coloring = "color "+textcolor+background
+    coloring = "color {0}{1}".format(self.colors[textcolor], self.colors[background])
     os.system(coloring)
+
 class linux():
   def __init__(self):
     self.colors = {"grey":'30',"red":'31', "green":'32',"yellow":'33',"blue":'34', 'magenta':'35','cyan':'36', 'white':'37'}
@@ -23,5 +24,6 @@ class linux():
     if other:
       text = "\033[{0}m{1}".format(self.other[other], text)
     return text + "\033[0m"
+
 
 
